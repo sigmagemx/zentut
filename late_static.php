b2f9d205be43220539f1a3b3b8ad4f03e2729acc
Evgenija <sigmagemx@gmail.com>
2017-03-07 11:44:47 +0300

    Finish static binding

---
+++ late_static.php
@@ -0,0 +1,14 @@
+<?php
+
+class Model {
+	protected static $tableName = 'Model';
+	public static function getTableName() {
+		return static::$tableName;
+	}
+}
+
+class User extends Model {
+	protected static $tableName = 'User';
+}
+
+echo User::getTableName();
