3ef22d982d46ed294f8b04a48e3ee3c092d8cfd5
Evgenija <sigmagemx@gmail.com>
2017-03-07 11:19:05 +0300

    Finish static methods

---
+++ static.php
@@ -0,0 +1,47 @@
+<?php
+
+class MyClass {
+	private static $static_property;
+
+	public static function static_method() {
+		//...
+	}
+
+	public function method() {
+		self::$static_property;
+		self::static_method();
+	}
+}
+
+class Counter {
+	private $count;
+	public static $instance;
+
+	public function __construct($count = 0) {
+		$this->count = $count;
+		self::$instance++;
+	}
+
+	public function count() {
+		$this->count++;
+		return $this;
+	}
+
+	public function __toString() {
+		return (string)$this->count;
+	}
+
+	public static function countInstance() {
+		return self::$instance;
+	}
+}
+
+$c1 = new Counter();
+$c1->count()->count();
+echo 'Counter 1 value: ' . $c1 . '<br/>';
+
+$c2 = new Counter();
+$c2->count()->count()->count();
+echo 'Counter 2 value: ' . $c2 . '<br/>';
+
+echo 'The number of counter instances:' . Counter::countInstance() . '<br>';
